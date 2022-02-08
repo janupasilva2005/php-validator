83ca1dae9ba2cdacd633635bd21a492b90122e9d
janupasilva <janupasilva@gmail.com>
2022-02-08 07:57:28 +0530

    Added phone,email,password validations

--- Validator.php
+++ Validator.php
@@ -1,6 +1,24 @@
 <?php
 
 /**
+ * 
+ * How to use this small library
+ * 
+ * 1. Create an instance
+ *      $validator = new Validator();
+ * 
+ * 2. Use any validator inside a try catch block
+ *      try {
+ *         $username = $validator->validateUsername(<name>, <min>, <max>);
+ * 
+ *          <DO ANYTHING WHAT YOU WANT WITH $username>
+ *      } catch(Exception $e) {
+ *          echo $e;
+ *      }
+ * 
+ */
+
+/**
  * Validator class
  */
 class Validator
@@ -35,6 +53,64 @@ class Validator
         {
             // If the validation is failed it will raise an error
             throw new Exception("Username cannot be more than: " . $max . " or less than: " . $min);
+        }
+    }
+
+    /**
+     * Function to validate passwords
+     */
+    public function validatePassword($password, $min, $max)
+    {
+        /**
+         * This will trime and remove all unnecessory characters in the string
+         */
+        $password = trim($password);
+        $password = stripslashes($password);
+        $password = strip_tags($password);
+
+        if(strlen($password) >= $min && strlen($password) <= $max)
+        {
+            return $password;
+        }
+        else
+        {
+            throw new Exception("Password cannot be more than: " . $max . " or less than: " . $min);
+        }
+    }
+
+    /**
+     * Function to validate emails
+     */
+    public function validateEmail($email)
+    {
+        $email = trim($email);
+
+        /**
+         * If the given email is not validated this will raise an exception
+         */
+        if(filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            return $email;
+        }
+        else
+        {
+            throw new Exception("Not validated email");
+        }
+    }
+
+    /**
+     * This will validate phone numbers
+     * param number - a string
+     */
+    public function validatePhone($number, $chars)
+    {
+        if(strlen($number) === $chars)
+        {
+            return $number;
+        }
+        else
+        {
+            throw new Exception("Not validated phone number");
         }
     }
 }
